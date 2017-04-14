5cc8117b495f0fb8b7b29a229410dce6f592451f
Sn0wFox <ruben.pericas@laposte.net>
2017-04-14 17:10:56 +0200

    Added some function to acces DB (pre book.available)

--- src/access/database.ts
+++ src/access/database.ts
@@ -8,6 +8,19 @@ function sanitizeUser(user: any): any {
   return user;
 }
 
+function sanitizeBookInfoForInsert(book: Book.Info): any {
+  delete book.id;
+  let s: string = '';
+  for(let genre of book.genres) {
+    s += genre + ',';
+  }
+  delete book.genres;
+  let b = Object.assign(book);
+  b.genres = s.substring(0, s.length - 1);
+  console.log(b);
+  return b;
+}
+
 export function getUserById(userId: User.ID): Bluebird<User.Info> {
   return Bluebird.resolve(Model.User
     .findById(userId))
@@ -35,10 +48,67 @@ export function getUserBooks(userID: User.ID): Bluebird<User.Books> {
     });
 }
 
+export function getUserAvailableBooks(userID: User.ID): Bluebird<User.Books> {
+  return Bluebird.resolve(Model.Book
+    .findAll({
+      where: {
+        userID: userID,
+        dateOfReturn: true
+      }
+    }))
+    .then((books: any[]) => {
+      let lib: User.Books = {
+        userID: userID,
+        booksID: []
+      };
+      for(let b of books) {
+        lib.booksID.push(b.dataValues.bookId);
+      }
+      return lib;
+    });
+}
+
 export function getBookById(bookID: Book.ID): Bluebird<Book.Info> {
   return Bluebird.resolve(Model.Book
     .findById(bookID))
     .then((book: any) => {
       return Object.assign(book.dataValues);
+    });
+}
+
+export function addBook(userID: User.ID, bookData: Book.Info | number | string): Bluebird<any> {  // TODO: type ID
+  return Bluebird
+    .try(() => {
+      if(typeof bookData == 'object') {
+        return Model
+          .Metadata.create(sanitizeBookInfoForInsert(bookData))
+          .then((res: any) => {
+            if(res) {
+              return Model.Book.create({userID: userID, bookID: res.dataValues.metaDataId});
+            }
+          });
+      }
+      return Model.Book.create({userID: userID, bookID: bookData});
+    });
+}
+
+export function addMetadata(book: Book.Info): Bluebird<any> {
+  // TODO: check for already existing metadata
+  return Bluebird.resolve(
+    Model.Metadata.create(sanitizeBookInfoForInsert(book))
+  );
+}
+
+export function getBookMetadata(bookID: Book.ID): Bluebird<Book.Info[]> {
+  return getBookById(bookID)
+    .then((book: any) => {
+      return Model.Metadata.find({
+        where: {
+          bookID: book.bookId
+        }
+      })
+    })
+    .map((data: any) => {
+      return data.dataValues;
     });
 }
