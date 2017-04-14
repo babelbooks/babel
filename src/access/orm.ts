0c5afdb74376df8075ba3ca1bc2d2a39690ec99d
Sn0wFox <ruben.pericas@laposte.net>
2017-04-14 10:20:25 +0200

    Added different configs for prod and dev env

--- src/access/orm.ts
+++ src/access/orm.ts
@@ -1,17 +1,36 @@
 import * as Sequelize from 'sequelize';
 
-let database = new Sequelize('BabelDB', 'root', 'admin', {
-  host: 'localhost',
-  dialect: 'mysql',
-  pool: {
-    max: 10,
-    min: 1,
-    idle: 10000
-  },
-  define: {
-    timestamps: false
+let options: Sequelize.Options
+
+if(process.env.NODE_ENV == 'production') {
+  options = {
+    host: 'localhost',
+    port: process.env.BB_DB_PORT,
+    dialect: 'mysql',
+    pool: {
+      max: process.env.BB_DB_MAXPOOL,
+      min: process.env.BB_DB_MINPOOL,
+      idle: process.env.BB_DB_IDLE
+    }
   }
-});
+} else {
+  options = {
+    host: 'localhost',
+    port: 3306,
+    dialect: 'mysql',
+    pool: {
+      max: 10,
+      min: 1,
+      idle: 10000
+    }
+  }
+}
+
+options.define = {
+  timestamps: false
+};
+
+let database = new Sequelize('BabelDB', 'root', 'admin', options);
 
 export namespace Model {
   export let User = database.import('./models/user');
