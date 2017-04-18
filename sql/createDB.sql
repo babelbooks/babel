7cc95956f41b364a01c406cae2d832255ba91938
Sn0wFox <ruben.pericas@laposte.net>
2017-04-18 19:40:15 +0200

    Updated database according to new model

--- sql/createDB.sql
+++ sql/createDB.sql
@@ -13,30 +13,23 @@ USE BabelDB;
 -- --------------------------------------------------------
 
 --
+-- User: the one thanks to whom our app will access babeldb
+--
+DROP USER IF EXISTS `borges`@`localhost`;
+CREATE USER `borges`@`localhost` IDENTIFIED BY 'devonly';
+GRANT INSERT, SELECT, UPDATE ON `BabelDB`.* TO `borges`@`localhost`;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `Book`
 --
 
 CREATE TABLE `Book` (
   `bookId` bigint(20) NOT NULL,
-  `bookMetaDataId` bigint(20) NOT NULL,
-  `userId` bigint(20) NOT NULL COMMENT 'Original owner',
+  `isbn` bigint(20) DEFAULT NULL,
+  `origin` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL COMMENT 'Original owner',
   `available` boolean NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `BookMetaData`
---
-
-CREATE TABLE `BookMetaData` (
-  `metaDataId` bigint(20) NOT NULL,
-  `isbn` varchar(20) DEFAULT NULL,
-  `title` varchar(200) NOT NULL,
-  `author` varchar(200) NOT NULL,
-  `edition` varchar(200) DEFAULT NULL,
-  `majorForm` varchar(200) DEFAULT NULL COMMENT 'ie Theatre, novel etc.',
-  `cover` varchar(2000) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
@@ -48,7 +41,7 @@ CREATE TABLE `BookMetaData` (
 CREATE TABLE `Borrow` (
   `borrowId` bigint(20) NOT NULL,
   `bookId` bigint(20) NOT NULL,
-  `userId` bigint(20) NOT NULL,
+  `userId` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
   `beginDate` timestamp NOT NULL,
   `dateOfReturn` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
@@ -60,29 +53,24 @@ CREATE TABLE `Borrow` (
 --
 
 CREATE TABLE `User` (
-  `userId` bigint(20) NOT NULL,
-  `username` varchar(45) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
-  `password` binary(64) NOT NULL,
-  `signUpDate` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `lastName` varchar(100) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
+  `username` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
+  `password` varchar(255) NOT NULL,
   `firstName` varchar(100) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
+  `lastName` varchar(100) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
   `points` int(11) NOT NULL,
-  `score` bigint(20) DEFAULT NULL
+  `score` bigint(20) DEFAULT NULL,
+  `signUpDate` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
 
 --
 -- Indexes for table `Book`
 --
 ALTER TABLE `Book`
   ADD PRIMARY KEY (`bookId`),
-  ADD KEY `bookMetaDataId` (`bookMetaDataId`),
-  ADD KEY `userId` (`userId`);
-
---
--- Indexes for table `BookMetaData`
---
-ALTER TABLE `BookMetaData`
-  ADD PRIMARY KEY (`metaDataId`);
+  ADD KEY `username` (`origin`),
+  ADD KEY `isbn` (`isbn`);
 
 --
 -- Indexes for table `Borrow`
@@ -90,55 +78,49 @@ ALTER TABLE `BookMetaData`
 ALTER TABLE `Borrow`
   ADD PRIMARY KEY (`borrowId`),
   ADD KEY `bookId` (`bookId`),
-  ADD KEY `userId` (`userId`);
+  ADD KEY `username` (`userId`);
 
 --
 -- Indexes for table `User`
 --
 ALTER TABLE `User`
-  ADD PRIMARY KEY (`userId`),
-  ADD KEY `userId` (`userId`);
+  ADD PRIMARY KEY (`username`),
+  ADD KEY `username` (`username`);
+
+-- --------------------------------------------------------
 
 --
 -- AUTO_INCREMENT for table `Book`
 --
 ALTER TABLE `Book`
   MODIFY `bookId` bigint(20) NOT NULL AUTO_INCREMENT;
---
--- AUTO_INCREMENT for table `BookMetaData`
---
-ALTER TABLE `BookMetaData`
-  MODIFY `metaDataId` bigint(20) NOT NULL AUTO_INCREMENT;
+
 --
 -- AUTO_INCREMENT for table `Borrow`
 --
 ALTER TABLE `Borrow`
   MODIFY `borrowId` bigint(20) NOT NULL AUTO_INCREMENT;
---
--- AUTO_INCREMENT for table `User`
---
-ALTER TABLE `User`
-  MODIFY `userId` bigint(20) NOT NULL AUTO_INCREMENT;
+
+-- --------------------------------------------------------
 
 --
 -- Constraints for table `Book`
 --
 ALTER TABLE `Book`
-  ADD CONSTRAINT `Book_ibfk_1` FOREIGN KEY (`bookMetaDataId`) REFERENCES `BookMetaData` (`metaDataId`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `Book_ibfk_2` FOREIGN KEY (`userId`) REFERENCES `User` (`userId`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `Book_ibfk_1` FOREIGN KEY (`origin`) REFERENCES `User` (`username`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Constraints for table `Borrow`
 --
 ALTER TABLE `Borrow`
   ADD CONSTRAINT `Borrow_ibfk_1` FOREIGN KEY (`bookId`) REFERENCES `Book` (`bookId`) ON DELETE NO ACTION ON UPDATE CASCADE,
-  ADD CONSTRAINT `Borrow_ibfk_2` FOREIGN KEY (`userId`) REFERENCES `User` (`userId`) ON DELETE NO ACTION ON UPDATE CASCADE;
+  ADD CONSTRAINT `Borrow_ibfk_2` FOREIGN KEY (`userId`) REFERENCES `User` (`username`) ON DELETE NO ACTION ON UPDATE CASCADE;
 
 DELIMITER $$
 
 CREATE FUNCTION newBorrowing
 (myBookId bigint,
- myUserId bigint
+ myUserId varchar(255)
 ) RETURNS BOOLEAN
 BEGIN
 
