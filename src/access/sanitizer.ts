670bb99ff8181fecb4f004ad09a2902fd72f97b7
Sn0wFox <ruben.pericas@laposte.net>
2017-04-21 15:37:42 +0200

    Added sanitizers' doc

--- src/access/sanitizer.ts
+++ src/access/sanitizer.ts
@@ -1,6 +1,12 @@
 import { User } from '../lib';
 import { Book } from '../lib';
 
+/**
+ * Ensures that the given user object
+ * can be safely broadcast.
+ * @param user The user to sanitize.
+ * @returns {User.Info}
+ */
 export function sanitizeUser(user: User.Info): User.Info {
   // The password must never be broadcast, so we delete it
   // The rest is fine
@@ -8,6 +14,12 @@ export function sanitizeUser(user: User.Info): User.Info {
   return user;
 }
 
+/**
+ * Ensures that the given user object
+ * can be safely inserted in the database.
+ * @param user The user to sanitize.
+ * @returns {User.Info}
+ */
 export function sanitizeUserForInsert(user: User.Info): any {
   // Delete values which can cause a mess and let database handle default values
   delete user.signUpDate;
@@ -16,6 +28,12 @@ export function sanitizeUserForInsert(user: User.Info): any {
   return user;
 }
 
+/**
+ * Ensures that the given book object
+ * can be safely inserted in the database.
+ * @param book The book to sanitize.
+ * @returns {Book.Raw}
+ */
 export function sanitizeBookForInsert(book: Book.Raw): any {
   // Delete values which can cause a mess and let database handle default values
   delete book.bookId;
