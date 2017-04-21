337acbddca83677d3bed7e4be49bda169640049e
Sn0wFox <ruben.pericas@laposte.net>
2017-04-21 12:06:34 +0200

    Added POST /logout and doc for authRouter

--- src/auth/auth.router.ts
+++ src/auth/auth.router.ts
@@ -6,6 +6,15 @@ import { NextFunction } from 'express';
 
 let router: Router = Router();
 
+/**
+ * POST /login
+ * username: string
+ * password: string
+ *
+ * Allows an user to log in, if he provides the right credentials.
+ * If so, it returns a 200 status code along with a small JSON object.
+ * Otherwise, it sends back a 400 status code.
+ */
 router.post('/login', (req: Request, res: Response, next: NextFunction) => {
   Passport.authenticate('local', (err: any, user: any, info: any) => {
     if(err || !user) {
@@ -21,6 +30,20 @@ router.post('/login', (req: Request, res: Response, next: NextFunction) => {
       authenticated: true
     });
   })(req, res, next);
+});
+
+/**
+ * POST /logout
+ *
+ * Allows an user to log out.
+ * If successful, it returns a 200 status code along with a small JSON object.
+ * This is not supposed to fail. If so, it therefore will send a 500 response.
+ */
+router.post('/logout', (req: Request, res: Response) => {
+  req.logout();
+  res.status(200).json({
+    loggedOut: true
+  });
 });
 
 export default router;
--- src/auth/auth.ts
+++ src/auth/auth.ts
@@ -1,5 +1,12 @@
 import { Request, Response, NextFunction } from 'express';
 
+/**
+ * The aim of this middleware is to protect restricted API endpoints.
+ * If the user is authenticated, the request is forwarded to the next middleware.
+ * Otherwise, it interrupts it and sends back to the user a 401 status code.
+ * The parameters are the same as the one needed by any express middleware.
+ * @returns {any}
+ */
 export function ensureAuthenticated(req: Request, res: Response, next: NextFunction): any {
   if(req.isAuthenticated()) {
     return next();
